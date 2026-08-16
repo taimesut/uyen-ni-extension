c9b134352ec0dba3cdab8c40015db2f35f0a8036
taimesut <133870468+taimesut@users.noreply.github.com>
2026-08-16 20:33:56 +0700

    remove birthday launcher from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
-import { BirthdayLauncher } from "./components/BirthdayLauncher";
 import { ToastContainer } from "./components/Toast";
 import { MobileLayout } from "./layouts/MobileLayout";
 import { DoiSoatPage } from "./pages/DoiSoatPage";
 import { FmsPage } from "./pages/FmsPage";
 import { HomePage } from "./pages/HomePage";
 import { SettingsPage } from "./pages/SettingsPage";
-import "./styles/birthday-clean.css";
 
 export const App = () => {
   return (
     <HashRouter>
-      <BirthdayLauncher />
       <ToastContainer />
       <MobileLayout>
         <Routes>
