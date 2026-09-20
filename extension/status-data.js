9efd7339e90149d724b00ff82cf0c5d6c9def99d
taimesut <133870468+taimesut@users.noreply.github.com>
2026-09-20 15:46:04 +0700

    feat(extension): add FMS status options

---
+++ extension/status-data.js
@@ -0,0 +1,19 @@
+"use strict";
+
+globalThis.SPX_FMS_STATUS_OPTIONS = [
+  { code: "15", name: "SOC_LHTransporting" },
+  { code: "36", name: "SOC_LHTransported" },
+  { code: "51", name: "Collected" },
+  { code: "879", name: "FMHub_LHArrived" },
+  { code: "880", name: "LMHub_LHArrived" },
+  { code: "881", name: "Hub_LHArrived" },
+  { code: "882", name: "SOC_LHArrived" },
+  { code: "914", name: "Hub_LHUnloading" },
+  { code: "916", name: "FMHub_LHUnloading" },
+  { code: "918", name: "SOC_LHUnloading" },
+  { code: "920", name: "LMHub_LHUnloading" },
+  { code: "924", name: "HUB_LHUnloaded" },
+  { code: "926", name: "FMHub_LHUnloaded" },
+  { code: "928", name: "SOC_LHUnloaded" },
+  { code: "930", name: "LMHub_LHUnloaded" }
+];
